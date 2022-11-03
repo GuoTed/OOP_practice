f80ba50a84ec804e5af4ac7c5c8655d97d97d2ac
GuoTed <tednogamenolife@gmail.com>
2022-11-03 16:23:13 +0800

    Inheritance

--- Investment.cpp
+++ Investment.cpp
@@ -4,11 +4,26 @@
 using namespace std;
 
 int main(){
+    // crete base class
     Item i(5, "1992");
     i.getprice();
     i.getpurprice();
     i.setprice(15);
-    i.getprofit();
+    i.getprofit(); // access failed bc it is protected 
+    i.getdes();
+    // create friend class
+    Income in;
+    cout << in.checkprofit(i) << endl; // 1: true
     // Polymorphism
+    ETF e(10, "1993");
+    e.getdes();
+    e.getcode();
+    e.setprice(20);
+    e.getprofitbyDerived(); // access is successful bc get through derived class
+    cout << in.checkprofit(e) << endl; // 0: false
+    // create Trade Strategies
+    Strategies s(10, "1993");
+    s.buy(e, in);
+    s.gettotalprofit();
 }
 
--- Investment.h
+++ Investment.h
@@ -4,13 +4,16 @@
 using namespace std;
 
 // Encapsulation
-// Item
+// Base class
 class Item{
+friend class Income;
 public:
-    Item(int x, string s){
+    // constructor
+    Item(int x, string s, string des="Item"){
         purchase_price = price = x;
         purchase_date = s;
-        cout << "create item" << endl;
+        description = des;
+        cout << "create " << des << endl;
     }
     void setprice(int newp){
         price = newp;
@@ -21,6 +24,10 @@ public:
     void getpurprice(){
         cout << "Purchase Price: " << purchase_price << endl;
     }
+    void getdes(){
+        cout << "Description: " << description << endl;
+    }
+protected:
     void getprofit(){
         cout << "Profit: " << price - purchase_price << endl;
     }
@@ -28,8 +35,73 @@ private:
     int price;
     int purchase_price;
     string purchase_date;
+    string description;
 };
 
-// to be cont...
+// Friend classes
+class Income{
+public:
+    bool checkprofit(Item i){
+        if(i.price - i.purchase_price>=0){
+            income = i.price - i.purchase_price;
+            return true;
+        }
+        else
+            return false;
+    }  
+    int getincome(){
+        return income;
+    }
+private:
+    int income;
+};
+
 // Inheritance
+// Derived class
+class ETF: public Item{
+public:
+    // constructor 
+    ETF(int price, string date, string des="ETF", string code="VTI"): Item(price, date, des){
+        // The keyword this names a pointer to the object 
+        // for which an implicit object member function ([class.mfct.non.static]) is invoked 
+        // or a non-static data member’s initializer ([class.mem]) is evaluated.
+        this->code = code;
+    }
+    void getcode(){
+        cout << "Code: " << code << endl;
+    }
+    void getprofitbyDerived(){
+        getprofit();
+    }
+private:
+    string code;
+};
+
+// Multiple Inheritance
+// can be extend by using template (ex: ETF / Found / Bond)
+class Strategies: public ETF, public Income{
+public:
+    Strategies(int price, string date, string des="ETF", string code="VTI"): ETF(price, date, des){
+        totalprofit = 0; // init
+    }
+    void buy(ETF e, Income in){
+        if(in.checkprofit(e)){
+            cout << "buy the ";
+            e.getcode();
+            totalprofit += in.getincome();
+        }
+        else{
+            cout << "Not now" << endl;
+        }
+    }
+    void gettotalprofit(){
+        cout << "Profit: " << totalprofit << endl;
+    }
+private:
+    int totalprofit;
+};
+
+
 // Polymorphism
+
+
